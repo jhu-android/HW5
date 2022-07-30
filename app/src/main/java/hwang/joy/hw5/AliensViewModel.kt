f681936282b5f82a23753c234a448c35e7e4763d
Joy Hwang <joyhwang10@gmail.com>
2022-07-30 13:38:14 -0400

    works

--- app/src/main/java/hwang/joy/hw5/AliensViewModel.kt
+++ app/src/main/java/hwang/joy/hw5/AliensViewModel.kt
@@ -8,64 +8,42 @@ import kotlinx.coroutines.flow.*
 class AliensViewModel: ViewModel() {
 
     private val alienAlerter = AlienAlerter(viewModelScope)
-
+    val activeUfos = alienAlerter.alerts.map { ImmutableList(it.ufos) }
+    val pointStore = pointStoreFlow()
 
     suspend fun startAlienReporting() {
         alienAlerter.startReporting()
     }
 
-    val activeUfos = alienAlerter.alerts.map { ImmutableList(it.ufos) }
+    private fun pointStoreFlow(): Flow<ImmutableMap<Int, ImmutableSet<LatLng>>> {
+        val mutableStore = mutableMapOf<Int, MutableSet<LatLng>>()
 
-    // Create Point Store
-    private val mutableStore = mutableMapOf<Int, MutableSet<LatLng>>()
-
-    private val alertStore =
-        alienAlerter.alerts.runningFold(mutableListOf<UfoPosition>()) { accumulator, alert ->
-            alert.ufos.forEach {
-                accumulator.add(it)
+        val alertStore =
+            alienAlerter.alerts.runningFold(mutableListOf<UfoPosition>()) { accumulator, alert ->
+                alert.ufos.forEach {
+                    accumulator.add(it)
+                }
+                accumulator
             }
-            accumulator
-        }
 
-
-    private val mutableStoreFlow = alertStore.map { alerts ->
-        alerts.forEach { alert ->
-            if (mutableStore.containsKey(alert.ship)) {
-                mutableStore[alert.ship]?.add(LatLng(alert.lat, alert.lon))
-            } else {
-                mutableStore[alert.ship] = mutableSetOf(LatLng(alert.lat, alert.lon))
+        val mutableStoreFlow =
+            alertStore.map { alerts ->
+                alerts.forEach { alert ->
+                    if (mutableStore.containsKey(alert.ship)) {
+                        mutableStore[alert.ship]?.add(LatLng(alert.lat, alert.lon))
+                    } else {
+                        mutableStore[alert.ship] = mutableSetOf(LatLng(alert.lat, alert.lon))
+                    }
+                }
+                mutableStore
             }
-        }
-        mutableStore
+
+        val mutableStoreListFlow =
+            mutableStoreFlow.map { flowValue ->
+                flowValue.mapValues { coordinates -> ImmutableSet(coordinates.value) }
+            }
+
+        return mutableStoreListFlow.map { ImmutableMap(it) }
     }
-
-    private val mutableStoreListFlow =
-        mutableStoreFlow.map { flowValue -> flowValue.mapValues { coordinates -> ImmutableSet(coordinates.value) } }
-
-    val pointStore = mutableStoreListFlow.map { ImmutableMap(it) }
 }
 
-
-
-//    private val _pointStore = MutableStateFlow(generatePointStore())
-//    val pointStore: Flow<Map<Int, ImmutableList<LatLng>>>
-//        get() = _pointStore
-//
-//    private fun generatePointStore(): Map<Int, ImmutableList<LatLng>> {
-//        Log.d("viewModel", "generatePointStore called")
-//        val mutableStore = mutableMapOf<Int, MutableList<LatLng>>()
-//
-//        alertStore.map { alerts ->
-//            alerts.forEach { alert ->
-//                if (mutableStore.containsKey(alert.ship)) {
-//                    mutableStore[alert.ship]?.add(LatLng(alert.lat, alert.lon))
-//                } else {
-//                    mutableStore[alert.ship] = mutableListOf(LatLng(alert.lat, alert.lon))
-//                }
-//            }
-//        }
-//
-//        val immutableCoordinates = mutableStore.mapValues { ImmutableList(it.value) }
-//        return immutableCoordinates.toMap()
-//    }
-//}
